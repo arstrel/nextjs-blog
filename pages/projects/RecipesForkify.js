9414f94751ce561cb195b27d0aa42c6b42a839cc
Artem Streltov <arstrel@gmail.com>
2020-08-01 20:42:26 -0400

    added recipe project description

--- pages/projects/RecipesForkify.js
+++ pages/projects/RecipesForkify.js
@@ -24,10 +24,10 @@ export default function RecipesForkify() {
         <GridItem xs={12} sm={12} md={6} className={classes.actionContainer}>
           <h1 className={classes.title}>Recipes with Forkify</h1>
           <h4 className={classes.smallDescription}>
-            A yoga studio in South Carolina had only a facebook page for web
-            presence and a big dream of making yoga classes affordable and
-            available for the local community. The owner wanted a way to create
-            custom daily routines for students of different ages and levels...
+            Recipes with Forkify allows you to search recipes from multiple
+            sources in one place. This little app can calculate the ammount of
+            ingredients according to desired servings, save the recipes in Liked
+            list persisted in localStorage and maintain shopping list.
           </h4>
           <br />
           <Button
@@ -61,45 +61,39 @@ export default function RecipesForkify() {
             tabContent: (
               <GridContainer justify="center">
                 <GridItem>
-                  Features two levels of user rights: admin and user. There is a
-                  custom daily yoga routine builder in the admin's zone that
-                  allows to create and assign the routines to individual users
-                  or to the entire group!
+                  When it comes to creating a website that needs to:
+                  <ul>
+                    <li>fetch the data from 3rd party API</li>
+                    <li>maintain multiple pieces of internal state</li>
+                    <li>
+                      frequently update different parts of the UI in response to
+                      user's actions
+                    </li>
+                  </ul>
+                  All too often it is tempting to reach out for heavy-hitters
+                  like React or Next. That is why the challenge of this project
+                  was to build a dynamic website with ES6 JavaScript and
+                  Model-View-Controller Architecture.
                 </GridItem>
                 <GridItem component="ul">
-                  <strong>Dynamic elements include:</strong>
+                  <p>This project has:</p>
                   <li>
-                    <i>Quotes: </i>
-                    rotate from predefined list at every home page load.
+                    Four isolated data models: Search, Recipe, Shopping list and
+                    Likes.
                   </li>
                   <li>
-                    <i>FAQs: </i>
-                    only available if logged in as an admin. Allows to
-                    add/delete/edit Q and A's directly from the page.
+                    Four corresponding view handlers that are responsible for
+                    adding/removing and updating the HTML.
                   </li>
                   <li>
-                    <i>Gallery: </i>
-                    allows to add/remove/edit pictures or youtube videos. Made
-                    with Cloudinary.
+                    Main controller that has all the logic and operates models
+                    and views.
                   </li>
-                  <li>
-                    <i>Class packages: </i>
-                    the users may express interest in purchasing class packages
-                    and the admin will see this in the admin's zone, together
-                    with user's email.
-                  </li>
-                  <li>Daily routine sequence builder.</li>
                 </GridItem>
                 <GridItem>
                   <p>
-                    <strong>Take a further look with demo credentials:</strong>
-                  </p>
-                  <p>
-                    <strong>Login: </strong>
-                    admin@admin.com
-                    <br />
-                    <strong>Password: </strong>
-                    admin
+                    All of the above is transpiled with Babel, bundled up
+                    together with Webpack and deployed on Github Pages.
                   </p>
                 </GridItem>
               </GridContainer>
@@ -110,15 +104,30 @@ export default function RecipesForkify() {
             tabIcon: Storefront,
             tabContent: (
               <GridContainer justify="center">
-                <GridItem>Powered by React.js</GridItem>
-                <GridItem className={classes.videoContainer}>
-                  <video width="80%" controls autoPlay>
-                    <source
-                      src="https://s3.amazonaws.com/media-p.slid.es/videos/1110040/VMcJYn9w/kukee-demo-sped-up.mp4"
-                      type="video/mp4"
-                    />
-                    Your browser does not support the video tag.
-                  </video>
+                <GridItem>Powered by HTML, CSS and JavaScript</GridItem>
+                <GridItem>
+                  <p>
+                    I used modern CSS tools to build this custom layout. Most of
+                    it is made with the use of CSS Grid and Flexbox. There are
+                    also some animations to make the website more interactive.
+                  </p>
+                  <p>
+                    As I use Webpack here, the website is served from /dist
+                    folder which contains the bundled code.
+                  </p>
+                  <p>
+                    All the UI manipulations are organized into four "view"
+                    files that take care of a corresponding parts of the
+                    website. Mainly, I make use of "insertAdjacentHTML" DOM
+                    function to insert a template string with the markup at the
+                    appropriate place in the UI.
+                  </p>
+                  <p>
+                    To detect clicks on the elements that are not yet there at
+                    the time of load, such as servings update button in the
+                    central part of a recipe's full view, I use ".matches" and
+                    ".closest" methods and event delegation.
+                  </p>
                 </GridItem>
               </GridContainer>
             ),
@@ -128,6 +137,27 @@ export default function RecipesForkify() {
             tabIcon: MenuBook,
             tabContent: (
               <GridContainer justify="center">
+                <GridItem>
+                  The data models are organized as ES6 classes that have some
+                  methods to manipulate the data stored inside.
+                  <p>
+                    Likes data is persisted in localStorage to ensure that saved
+                    recipes are not lost for the user.
+                  </p>
+                  <p>
+                    One of the most challenging and tricky (and probably buggy)
+                    parts was the calculation of the amounts of ingredients for
+                    the recipe. The recipes come from different sources and do
+                    not have the same structure. Most of the ingredients use
+                    fractions to indicate amounts necessary, some of them have
+                    units and some do not. So the tricky part was to parse the
+                    ingredients to separate the amounts from units and from the
+                    rest of the ingredient. After that the app stores this data
+                    separately in an object with decimal amounts to enable
+                    calculations. For the UI this data is changed once again to
+                    fractions.
+                  </p>
+                </GridItem>
                 <GridItem>
                   <p>
                     <a href="https://github.com/arstrel/recepies/">
